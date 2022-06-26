f37a3b4be60d363d6444baaeb7cb542d8c2b5a7d
Jun Kubota <kinkado115@gmail.com>
2022-06-26 20:43:26 +0900

    flake8対応

--- script/some_function.py
+++ script/some_function.py
@@ -8,25 +8,28 @@ def softmax(a):
     y = exp_a / sum_exp_a
     return y
 
-    
+
 def step_function(x):
     return np.array(x > 0, dtype=np.int64)
-
 
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+
 def relu(x):
     return np.maximum(0, x)
 
+
 def mean_squared_error(y, t):
     return 0.5 * np.sum((y - t) ** 2)
+
 
 def identity_function(x):
     if type(x) == str:
         raise TypeError(x, "This is a string!")
     return x
+
 
 def cross_entropy_error(y, t):
     # Where, we plus delta because np.log(0) is negative infinity that stop a model from proceeding this process.
